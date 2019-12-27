587aa307cb3c101467def91b842bf0387aed07b4
Dasarada Ram Reddy <dachi8958@gmail.com>
2019-12-27 20:34:46 +0530

    Components files

---
+++ src/Components/EditPosition.component.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class EditPosition extends Component{
+	render(){
+		return(
+			<div>
+				<h1>EditPosition</h1>
+			</div>
+			);
+	}
+}
+
+export default EditPosition;
---
+++ src/Components/PositionList.component.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class PositionList extends Component
+{
+	render(){
+		return(
+			<div>
+				<h1>PositionList</h1>
+			</div>
+			);
+	}
+}
+
+export default PositionList;
